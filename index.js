10b0e6d7af2b1d2cb90bba4bf5e7d6d6dc834fb2
closedwall <yadavrajesh5612@gmail.com>
2023-10-23 22:53:50 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import http from 'http';
+import url from 'url'
+import fs from 'fs'
+import formidable from 'formidable';
+
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(async(req,res)=>{
+    if(req.url === '/fileupload' && req.method.toLowerCase() === 'post'){
+        var form = formidable()
+        let fields;
+        let files;
+        try {
+            [fields, files] = await form.parse(req);
+        } catch (error) {
+            console.log("error",error);
+            return;
+        }
+            let oldpath = files.filetoupload.path;
+            console.log(files.filetoupload);
+            let newpath ='C:/users/yadav/'+files.filetoupload.originalFilename;
+            fs.rename(oldpath,newpath,(err)=>{
+                if(err)throw err;
+                res.write('File uploaded and moved!');
+                res.end();
+            });
+    }else{
+        res.writeHead(200,{"Content-Type":"text/html"});
+        res.write('<form action="fileupload" method="post" enctype="multipart/form-data" >');
+        res.write('<input type="file" name="filetoupload"><br> ');
+        res.write('<input type="submit">');
+        res.write('</form>');
+        return res.end();
+    }
+});
+
+server.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
+})
